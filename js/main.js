c5aed437a06b981dae7eae2020cb84dd5030e1c0
jnicholz <10808080@my.uvu.edu>
2021-11-12 23:48:12 -0700

    Actual Changes

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
 var currentView = "home";
 var Nav = "homeNav"
-changeArea(currentView, Nav)
-  function changeImage(imgName)
-  {
-     image = document.getElementById('displayed');
-     image.src = imgName;
-  }
+$(document).ready(function(){
+
+
+  changeArea(currentView, Nav);
+
+
+$('.imageThumb').click(function(){
+
+  let src=$(this).attr("src");
+   $("#displayed").attr("src", src);
   
-/* function changeImage(a) {
-    document.getElementById("img").src=a} */
+})
+
+
+ /*function changeImage(a) {
+    document.getElementById("img").src=a
+  }*/
+
+})
+
+
+
 function changeArea(to, newNav){
     
-    document.getElementById(currentView).style.display="none";
-    document.getElementById(Nav).style.background ="DodgerBlue"
-    document.getElementById(to).style.display="flex";
-    document.getElementById(newNav).style.background="lightgreen"
-    currentView = to
-    Nav = newNav
+  document.getElementById(currentView).style.display="none";
+  document.getElementById(Nav).style.background ="DodgerBlue"
+  document.getElementById(to).style.display="flex";
+  document.getElementById(newNav).style.background="indigo"
+  currentView = to
+  Nav = newNav
 }
-
-
