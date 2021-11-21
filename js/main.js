17f6e942f2af632c93034582a5bceb0fd5e7a0a5
jnicholz <10808080@my.uvu.edu>
2021-11-20 23:57:31 -0700

    Lot's of updates

--- js/main.js
+++ js/main.js
@@ -1,26 +1,56 @@
-var currentView = "home";
-var Nav = "homeNav"
+/*
+Added the Nav item in Jquery, Theme, and "imageThumb"  To change the displayed images*/
+
+var currentView = "#home";
+var Nav = "#homeNav";
+var toggleTheme ="css/theme.css";
+var selColor = "#0940E3";
+var unSelColor ="#178EFB";
+var defSelColor = "#E33834";
+var defUnSel = "#ED42D3";
+
+
+
 $(document).ready(function(){
 
 
-  changeArea(currentView, Nav);
+  $("#homeNav").css("background-color", selColor);
 
 
 $('.imageThumb').click(function(){
-
   let src=$(this).attr("src");
    $("#displayed").attr("src", src);
   
+});
+
+$('#Theme').click(function (){
+  let temp = $('#Stacker').attr('href');
+  $('#Stacker').attr('href',toggleTheme);
+  toggleTheme = temp;
+  
+  //This section trades the colors for selected nav items
+  temp = selColor;
+  let temp2 = unSelColor;
+  selColor = defSelColor;
+  unSelColor = defUnSel;
+  defSelColor = temp;
+  defUnSel =temp2;
+  
+});
+
+$('.navItem').click(function(){
+ 
+  $(this).css("background-color",selColor);
+  $(Nav).css("background-color", unSelColor);
+  $(currentView).css("display","none");
+  currentView=$(this).attr("area");
+  Nav = ("#"+$(this).attr("id"));
+  $(currentView).css("display", "flex");
+  });
+
 })
 
-
- /*function changeImage(a) {
-    document.getElementById("img").src=a
-  }*/
-
-})
-
-
+/*
 
 function changeArea(to, newNav){
     
@@ -31,3 +61,4 @@ function changeArea(to, newNav){
   currentView = to
   Nav = newNav
 }
+*/
